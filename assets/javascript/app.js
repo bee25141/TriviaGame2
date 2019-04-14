1a8877ba1ccf8aff6f225b26e23edb405707e6ad
bee25141 <bee25141@gmail.com>
2019-04-13 22:54:12 -0500

    finished up project for submission

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,23 +8,30 @@ $("document").ready(function () {
 
     //This function sets the question timer
     var n = 120;
+
     function questionTimer() {
         n--;
         if (n > 0) {
             setTimeout(questionTimer, 1000);
         }
         $("#timeLeft").text("Time Left: " + n);
-        if (n === 0){
+        //This calculates the score at the end of the game
+        if (n === 0) {
             console.log("timer 0");
-            $(".qa").hide();
             $("#timeLeft").hide();
-            $("#content").hide();
             $(".scoreboard").show();
-            $(".scoreboard").text("TIME IS UP");
-            $(".scoreboard").append("<br>" + "Answers Correct: " + answersCorrect)
-            $(".scoreboard").append("<br>" + "Answers Wrong: " + answersIncorrect)
+            $(".scoreboard").text("DYNAMITE");
+            $(".scoreboard").append("<br>" + "Answers Correct: " + answersCorrect);
+            $(".scoreboard").append("<br>" + "Answers Wrong: " + answersIncorrect);
+            $(".scoreboard").append("<br>" + "<button>" + "Try Again?").addClass("reset");
+
+            //This calls the reset function upon user click
+            $(".reset").on("click", function () {
+                console.log("click");
+                reset();
+            });
         }
-    }
+    };
     //This function starts the game when user clicks start
     $("#start").on("click", function () {
         $("#start").hide();
@@ -33,99 +40,115 @@ $("document").ready(function () {
         questionTimer();
     });
 
-    $(".answers1").one("click", function(){
+    function reset() {
+        answersCorrect = 0;
+        answersIncorrect = 0;
+        n = 120;
+        questionTimer();
+        $("#timeLeft").show();
+        $(".scoreboard").hide();
+        console.log(answersCorrect, answersIncorrect);
+    };
+
+
+
+    //This logs the users answers against the correct answers
+    $(".answers1").on("click", function () {
         var userGuess = ($(this).val());
-        if (userGuess == 3){
+        console.log(answersCorrect, answersIncorrect);
+        if (userGuess == 3) {
             answersCorrect++;
-        }
-        else if (userGuess !== 3){
+        } else if (userGuess !== 3) {
             answersIncorrect++;
         }
     });
-    $(".answers2").on("click", function(){
+    $(".answers2").on("click", function () {
         var userGuess = ($(this).val());
-        if (userGuess == 4){
+        console.log(answersCorrect, answersIncorrect);
+        if (userGuess == 4) {
             answersCorrect++;
-        }
-        else if (userGuess !== 4){
+        } else if (userGuess !== 4) {
             answersIncorrect++;
         }
     });
-    $(".answers3").on("click", function(){
+    $(".answers3").on("click", function () {
         var userGuess = ($(this).val());
-        if (userGuess == 4){
+        console.log(answersCorrect, answersIncorrect);
+        if (userGuess == 4) {
             answersCorrect++;
-        }
-        else if (userGuess !== 4){
+        } else if (userGuess !== 4) {
             answersIncorrect++;
         }
     });
-    $(".answers4").on("click", function(){
+    $(".answers4").on("click", function () {
         var userGuess = ($(this).val());
-        if (userGuess == 2){
+        console.log(answersCorrect, answersIncorrect);
+        if (userGuess == 2) {
             answersCorrect++;
-        }
-        else if (userGuess !== 2){
+        } else if (userGuess !== 2) {
             answersIncorrect++;
         }
     });
-    $(".answers5").on("click", function(){
+    $(".answers5").on("click", function () {
         var userGuess = ($(this).val());
-        if (userGuess == 2){
+        console.log(answersCorrect, answersIncorrect);
+        if (userGuess == 2) {
             answersCorrect++;
-        }
-        else if (userGuess !== 2){
+        } else if (userGuess !== 2) {
             answersIncorrect++;
         }
     });
-    $(".answers6").on("click", function(){
+    $(".answers6").on("click", function () {
         var userGuess = ($(this).val());
-        if (userGuess == 1){
+        console.log(answersCorrect, answersIncorrect);
+        if (userGuess == 1) {
             answersCorrect++;
-        }
-        else if (userGuess !== 1){
+        } else if (userGuess !== 1) {
             answersIncorrect++;
         }
     });
-    $(".answers7").on("click", function(){
+    $(".answers7").on("click", function () {
         var userGuess = ($(this).val());
-        if (userGuess == 2){
+        console.log(answersCorrect, answersIncorrect);
+        if (userGuess == 2) {
             answersCorrect++;
-        }
-        else if (userGuess !== 2){
+        } else if (userGuess !== 2) {
             answersIncorrect++;
         }
     });
-    $(".answers8").on("click", function(){
+    $(".answers8").on("click", function () {
         var userGuess = ($(this).val());
-        if (userGuess == 3){
+        console.log(answersCorrect, answersIncorrect);
+        if (userGuess == 3) {
             answersCorrect++;
-        }
-        else if (userGuess !== 3){
+        } else if (userGuess !== 3) {
             answersIncorrect++;
         }
     });
-    $(".answers9").on("click", function(){
+    $(".answers9").on("click", function () {
         var userGuess = ($(this).val());
-        if (userGuess == 3){
+        console.log(answersCorrect, answersIncorrect);
+        if (userGuess == 3) {
             answersCorrect++;
-        }
-        else if (userGuess !== 3){
+        } else if (userGuess !== 3) {
             answersIncorrect++;
         }
     });
-    $(".answers10").on("click", function(){
+    $(".answers10").on("click", function () {
         var userGuess = ($(this).val());
-        if (userGuess == 1){
+        console.log(answersCorrect, answersIncorrect);
+        if (userGuess == 1) {
             answersCorrect++;
-        }
-        else if (userGuess !== 1){
+        } else if (userGuess !== 1) {
             answersIncorrect++;
         }
         console.log(answersCorrect, answersIncorrect);
     });
-    $("#submit").on("click", function(){
+
+    //This ends the game
+    $("#submit").on("click", function () {
         n = 1;
+        window.scrollTo(0, 0);
     });
 
 });
